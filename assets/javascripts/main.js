5756ecdb4f97eeaa5de4357a1cfa4eef4ad347cb
bkeyvani <webmaster@keyvani.com>
2014-08-18 17:44:00 -0400

    add form validation

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,1 +1,53 @@
-console.log('This would be the main JS file.');
+
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (!re.test(email.val())) {
+    email.addClass("error").attr("aria-invalid",true);
+  }
+  else {
+    $(email).removeClass("error").removeAttr("aria-invalid"); 
+  }
+  
+  return re.test(email.val());
+}
+
+function validatePhone(phone) {
+  var re = /^\d{10}$/;
+  if (!re.test(phone.val())) {
+    phone.addClass("error").attr("aria-invalid",true);
+  }
+  else {
+    $(phone).removeClass("error").removeAttr("aria-invalid"); 
+  }
+  
+  return re.test(phone.val());
+}
+
+function validateForm (e) {
+    if (!e.value) {
+      $(e).addClass("required").attr("aria-invalid",true);
+    }
+    else {
+      $(e).removeClass("required").removeAttr("aria-invalid");
+    }
+
+    // validate email field
+    if (e.id === "entry_809307019") {
+      validateEmail($(e));
+    }
+
+    // validate phone field
+    if (e.id === "entry_843662138") {
+      validatePhone($(e));
+    }
+}
+
+$(document).ready(function() {
+  // add evenlistener to form fields on blur
+  $("input[required]").blur(function() {
+    validateForm(this);
+ });
+
+  // focus on first name input
+  $("#entry_1562922550").focus();
+});
