f56bf9051026b5b2c882f3f5229ccb660c0236a2
bkeyvani <webmaster@keyvani.com>
2014-10-14 15:11:04 -0400

    updated files to improve site performance

--- assets/javascripts/smooth-scroll.js
+++ assets/javascripts/smooth-scroll.js
@@ -1,17 +1,19 @@
 // Code from CSS-Tricks, Smooth Scrolling
 // http://css-tricks.com/snippets/jquery/smooth-scrolling/
 
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 500);
-        return false;
+if (window.jQuery) {
+  $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 500);
+          return false;
+        }
       }
-    }
+    });
   });
-});
+}
